f24adbff6fbb7fdace303da72e8dccaa0461edea
Talley Lambert <talley.lambert@gmail.com>
2022-02-09 16:52:45 -0500

    add tests

--- psygnal/containers/__init__.py
+++ psygnal/containers/__init__.py
@@ -1,10 +1,9 @@
 """Containers backed by psygnal events."""
 from ._evented_list import EventedList
-from ._evented_proxy import EventedCallableObjectProxy, EventedObjectProxy
+from ._evented_proxy import EventedObjectProxy
 from ._evented_set import EventedOrderedSet, EventedSet, OrderedSet
 
 __all__ = [
-    "EventedCallableObjectProxy",
     "EventedList",
     "EventedObjectProxy",
     "EventedOrderedSet",
