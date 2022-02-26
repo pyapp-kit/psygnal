9039461e1b27022d27b13afb36803d69868822cb
Alister Burt <alisterburt@gmail.com>
2022-02-26 22:14:28 +0000

    add active tests for selection

--- tests/containers/test_selection.py
+++ tests/containers/test_selection.py
@@ -3,17 +3,33 @@ from unittest.mock import Mock
 from psygnal.containers._selection import Selection
 
 
-def test_selection():
-    sel = Selection()
-    sel.events._current = Mock()
-    assert not sel._current
-    assert not sel
-    sel.add(1)
-    sel._current = 1
-    sel.events._current.emit.assert_called_once()
+def test_add_and_remove_from_selection():
+    selection = Selection()
+    selection.events._current = Mock()
+    assert not selection._current
+    assert not selection
+    selection.add(1)
+    selection._current = 1
+    selection.events._current.emit.assert_called_once()
 
-    assert 1 in sel
-    assert sel._current == 1
+    assert 1 in selection
+    assert selection._current == 1
 
-    sel.remove(1)
-    assert not sel
+    selection.remove(1)
+    assert not selection
+
+
+def test_selection_update_active_called_on_selection_change():
+    selection = Selection()
+    selection._update_active = Mock()
+    selection.add(1)
+    selection._update_active.assert_called_once()
+
+
+def test_selection_active_event_emitted_on_selection_change():
+    selection = Selection()
+    selection.events.active = Mock()
+    assert not selection.active
+    selection.add(1)
+    assert selection.active == 1
+    selection.events.active.emit.assert_called_once()
