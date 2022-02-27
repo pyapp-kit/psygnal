b9f373b9da83f5b3dfa0bd3725316c6c66cdc9f7
Talley Lambert <talley.lambert@gmail.com>
2022-02-27 15:57:26 -0500

    finish coverage

--- tests/test_throttler.py
+++ tests/test_throttler.py
@@ -6,7 +6,7 @@ from psygnal._throttler import debounced, throttled
 
 def test_debounced() -> None:
     mock1 = Mock()
-    f1 = debounced(mock1, timeout=5)
+    f1 = debounced(mock1, timeout=5, leading=False)
     f2 = Mock()
 
     [(f1(), f2()) for _ in range(10)]
@@ -17,13 +17,37 @@ def test_debounced() -> None:
     assert f2.call_count == 10
 
 
-def test_throttled() -> None:
+def test_debounced_leading() -> None:
     mock1 = Mock()
-    f1 = throttled(mock1, timeout=5)
+    f1 = debounced(mock1, timeout=5, leading=True)
     f2 = Mock()
 
     [(f1(), f2()) for _ in range(10)]
 
     time.sleep(0.008)
     assert mock1.call_count == 2
+    assert f2.call_count == 10
+
+
+def test_throttled() -> None:
+    mock1 = Mock()
+    f1 = throttled(mock1, timeout=5, leading=True)
+    f2 = Mock()
+
+    [(f1(), f2()) for _ in range(10)]
+
+    time.sleep(0.008)
+    assert mock1.call_count == 2
+    assert f2.call_count == 10
+
+
+def test_throttled_trailing() -> None:
+    mock1 = Mock()
+    f1 = throttled(mock1, timeout=5, leading=False)
+    f2 = Mock()
+
+    [(f1(), f2()) for _ in range(10)]
+
+    time.sleep(0.008)
+    assert mock1.call_count == 1
     assert f2.call_count == 10
