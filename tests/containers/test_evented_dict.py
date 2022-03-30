8f5c4d7320422dc377cde20824e5fc9da321cfd0
Alister Burt <alisterburt@gmail.com>
2022-03-30 14:56:05 +0100

    newlike test

--- tests/containers/test_evented_dict.py
+++ tests/containers/test_evented_dict.py
@@ -27,6 +27,7 @@ def test_dict(regular_dict):
         ("__delitem__", ("A",), ("removing", "removed")),  # delete
         ("__len__", (), 3),
         ("__repr__", (), "{'A': 1, 'B': 2, 'C': 3}"),
+        ("__newlike__", ({"A": 1},), {"A": 1}),
     ],
 )
 def test_dict_interface_parity(regular_dict, test_dict, method_name, args, expected):
