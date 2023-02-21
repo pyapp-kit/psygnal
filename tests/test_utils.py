09f69add3ffa4c5470114ff31eb61406376d0347
Talley Lambert <talley.lambert@gmail.com>
2023-02-21 13:05:52 -0500

    test: skip all on windows

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -94,10 +94,7 @@ def test_monitor_all() -> None:
     ]
 
 
-OLD_WIN = bool((sys.version_info < (3, 8)) and os.name == "nt")
-
-
-@pytest.mark.skipif(OLD_WIN, reason="can't rewrite open files on Windows")
+@pytest.mark.skipif(os.name == "nt", reason="rewrite open files on Windows is buggy")
 def test_decompile_recompile(monkeypatch):
     import psygnal
 
