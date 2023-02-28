9ec35905c39e1bc34d0cc79667c08ee6dc053386
Grzegorz Bokota <bokota+github@gmail.com>
2023-02-28 15:14:30 +0100

    build: add pyinstaller hook to simplify frozing apps using pyinstaller  (#194)
    * add pyinstaller hook
    
    * fix test
    
    * fix for older python version
    
    * try different approach of detect so files
    
    * add test for hiddenimports
    
    * skip test with uncompiled
    
    * add info about content of psygnal dir
    
    * fix hiddenimport workaround
    
    * remove obsolete info in test
    
    * filter warning on pyinstaller import
    
    * filter warning on pyinstaller import
    
    * try better filter BackendFilter
    
    * try better filter BackendFilter2
    
    * try better filter BackendFilter3
    
    * dellay import
    
    * filter warnings different approach
    
    * run part of test_hook_content without compilation
    
    * ommit coverage of hook-psygnal

---
+++ src/psygnal/_pyinstaller_util/hook-psygnal.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from typing import Iterable, List, Union
+
+try:
+    from importlib.metadata import PackageNotFoundError, PackagePath
+    from importlib.metadata import files as package_files
+except ImportError:
+    from importlib_metadata import (  # type: ignore[no-redef]
+        PackageNotFoundError,
+        PackagePath,
+    )
+    from importlib_metadata import files as package_files  # type: ignore[no-redef]
+
+try:
+    import psygnal
+
+    PSYGNAL_DIR = Path(psygnal.__file__).parent
+except ImportError:
+    PSYGNAL_DIR = Path(__file__).parent.parent
+
+
+def binary_files(file_list: Iterable[Union[PackagePath, Path]]) -> List[Path]:
+    return [Path(file) for file in file_list if file.suffix in {".so", ".pyd"}]
+
+
+def create_hiddenimports() -> List[str]:
+    res = ["mypy_extensions", "__future__"]
+
+    try:
+        files_list = package_files("psygnal")
+    except PackageNotFoundError:
+        return res
+
+    if files_list is None:
+        return res
+
+    modules = binary_files(files_list)
+
+    if len(modules) < 2:
+        # This is a workaround for a bug in importlib.metadata in editable mode
+
+        src_path = PSYGNAL_DIR.parent
+
+        modules = [
+            x.relative_to(src_path)
+            for x in binary_files(PSYGNAL_DIR.iterdir())
+            + binary_files(src_path.iterdir())
+        ]
+
+    for module in modules:
+        res.append(str(module).split(".")[0].replace("/", ".").replace("\\", "."))
+
+    return res
+
+
+hiddenimports = create_hiddenimports()
---
+++ tests/test_pyinstaller_hook.py
@@ -0,0 +1,55 @@
+import importlib.util
+import os
+import subprocess
+import warnings
+from pathlib import Path
+
+import psygnal
+
+
+def test_hook_content():
+    spec = importlib.util.spec_from_file_location(
+        "hook",
+        os.path.join(
+            os.path.dirname(psygnal.__file__), "_pyinstaller_util", "hook-psygnal.py"
+        ),
+    )
+    hook = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hook)
+
+    assert "mypy_extensions" in hook.hiddenimports
+
+    if not psygnal._compiled:
+        return
+
+    assert "psygnal._dataclass_utils" in hook.hiddenimports
+
+
+def test_pyintstaller_hiddenimports(tmp_path: Path) -> None:
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+
+        from PyInstaller import __main__ as pyi_main
+
+    build_path = tmp_path / "build"
+    dist_path = tmp_path / "dist"
+    app_name = "psygnal_test"
+    app = tmp_path / (app_name + ".py")
+    app.write_text("\n".join(["import psygnal", "print(psygnal.__version__)"]))
+
+    args = [
+        # Place all generated files in ``tmp_path``.
+        "--workpath",
+        str(build_path),
+        "--distpath",
+        str(dist_path),
+        "--specpath",
+        str(tmp_path),
+        str(app),
+    ]
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # silence warnings about deprecations
+        pyi_main.run(args)
+    subprocess.run([str(dist_path / app_name / app_name)], check=True)
