fdcd4d888f6785fa14afc57f279111f163ddc643
getzze <getzze@gmail.com>
2024-02-01 23:48:13 +0000

    correct error

--- src/psygnal/_evented_decorator.py
+++ src/psygnal/_evented_decorator.py
@@ -32,6 +32,7 @@ def evented(
     equality_operators: Optional[Dict[str, EqOperator]] = None,
     warn_on_no_fields: bool = ...,
     cache_on_instance: bool = ...,
+    alias_private_fields: Optional[bool] = ...,
     signal_suffix: str = ...,
 ) -> T:
     ...
@@ -45,6 +46,7 @@ def evented(
     equality_operators: Optional[Dict[str, EqOperator]] = None,
     warn_on_no_fields: bool = ...,
     cache_on_instance: bool = ...,
+    alias_private_fields: Optional[bool] = ...,
     signal_suffix: str = ...,
 ) -> Callable[[T], T]:
     ...
