8f44f34dd5818fae8fa56623cfb5e3e3ad139d99
Talley Lambert <talley.lambert@gmail.com>
2024-02-24 09:32:32 -0500

    chore: remove asynchronous emit and other deprecations (#282)
    * chore: remove asynchronous emit and other deprecations
    
    * fix: typesafety test

--- tests/test_psygnal.py
+++ tests/test_psygnal.py
@@ -1,5 +1,4 @@
 import gc
-import time
 from contextlib import suppress
 from functools import partial, wraps
 from inspect import Signature
@@ -9,7 +8,7 @@ from unittest.mock import MagicMock, Mock, call
 import pytest
 import toolz
 
-from psygnal import EmitLoopError, Signal, SignalInstance, _compiled
+from psygnal import EmitLoopError, Signal, SignalInstance
 from psygnal._weak_callback import WeakCallback
 
 
@@ -581,34 +580,6 @@ def test_unique_connections():
 
     e.one_int.connect(f_no_arg)
     assert len(e.one_int._slots) == 2
-
-
-@pytest.mark.skipif(_compiled, reason="passes, but segfaults on exit")
-def test_asynchronous_emit():
-    e = Emitter()
-    a = []
-
-    def slow_append(arg: int):
-        time.sleep(0.1)
-        a.append(arg)
-
-    mock = MagicMock(wraps=slow_append)
-    e.no_arg.connect(mock, unique=False)
-
-    assert not Signal.current_emitter()
-    value = 42
-    with pytest.warns(FutureWarning):
-        thread = e.no_arg.emit(value, asynchronous=True)
-    mock.assert_called_once()
-    assert Signal.current_emitter() is e.no_arg
-
-    # dude, you have to wait.
-    assert not a
-
-    if thread:
-        thread.join()
-    assert a == [value]
-    assert not Signal.current_emitter()
 
 
 def test_sig_unavailable():
