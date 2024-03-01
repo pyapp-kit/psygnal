bfc665ef09687f1af2c7489a6e0085434af01def
Talley Lambert <talley.lambert@gmail.com>
2024-02-29 19:01:36 -0500

    minor name change

--- src/psygnal/_evented_decorator.py
+++ src/psygnal/_evented_decorator.py
@@ -119,9 +119,7 @@ def evented(
         if not isinstance(cls, type):  # pragma: no cover
             raise TypeError("evented can only be used on classes")
         if any(k.startswith("_psygnal") for k in getattr(cls, "__annotations__", {})):
-            raise TypeError(
-                "Attributes on an evented class cannot start with '_psygnal'"
-            )
+            raise TypeError("Fields on an evented class cannot start with '_psygnal'")
 
         descriptor = SignalGroupDescriptor(
             equality_operators=equality_operators,
