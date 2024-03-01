426d9863fb2b00275e155fa51454cf34074b93b6
Talley Lambert <talley.lambert@gmail.com>
2024-02-29 19:00:59 -0500

    use annotations rather than __dict__

--- src/psygnal/_evented_decorator.py
+++ src/psygnal/_evented_decorator.py
@@ -118,7 +118,7 @@ def evented(
     def _decorate(cls: T) -> T:
         if not isinstance(cls, type):  # pragma: no cover
             raise TypeError("evented can only be used on classes")
-        if any(k.startswith("_psygnal") for k in getattr(cls, "__dict__", {})):
+        if any(k.startswith("_psygnal") for k in getattr(cls, "__annotations__", {})):
             raise TypeError(
                 "Attributes on an evented class cannot start with '_psygnal'"
             )
